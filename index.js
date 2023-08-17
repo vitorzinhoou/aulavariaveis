623a248d6c74a9c1dbc84dc50c2067ba05367eae
Rafaelcamarg <rafaelcamargo06@gmail.com>
2023-08-16 21:49:24 -0300

    1,2 e 3

---
+++ index.js
@@ -0,0 +1,38 @@
+exercicio 1
+
+let a = 10
+let b = 10
+
+console.log(b)
+
+b=5
+
+console.log=(a, b)
+
+resposta:VAI IMPRIMIR
+10
+10 5
+
+exercicio 2
+
+leta=10
+letb=20
+c = a
+b = c
+a = b
+
+console.log(a , b , c )
+
+resposta:vai IMPRIMIR
+
+10 10 10
+
+exercicio 3
+
+resposta:
+
+let TrabalhaQuantasHoras = prompt ("quantas horas voçe trabalha por dia")
+let  RecebeDia = prompt ("quanto voçe recebe por dia")
+
+const nome =
+let idade =
